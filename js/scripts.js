2d1a09ccf0c74917989f2db169834acecb4189c7
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-29 14:45:42 -0800

    clean up code and fix bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Business Logic
-function Pizza(topping, size,) {
-  this.topping = topping;
-  this.size = size;
+function Pizza() {
+  this.topping = "";
+  this.size = "";
   this.toppings = [];
   this.totalCost = 0;
 }
@@ -12,7 +12,7 @@ Pizza.prototype.pizzaCost = function () {
     this.totalCost = this.totalCost + 3;
     console.log(this.totalCost)
   }
-
+console.log(this.size)
   return ( this.size === "Small" ? this.totalCost += 10
       : this.size === "Medium" ? this.totalCost += 14
         : this.size === "Large" ? this.totalCost += 20
@@ -25,8 +25,7 @@ Pizza.prototype.createPizza = function () {
 //UI Logic
 let currentSize = ""
 let toppingArr = ["cheese", "pepperoni", "olives", "fried chicken", "banana", "chicken livers", "hot pocket", "mushrooms", "candy canes", "pineapple"]
-let newPizza = new Pizza("cheese", "medium");
-let pizzaList = {}
+let newPizza = new Pizza();
 let clickCounter = 0;
 
 $(document).ready(function () {
@@ -42,6 +41,7 @@ $(document).ready(function () {
   $("#btn-small, #btn-medium, #btn-large, #btn-XL").click(function (event) {
     let clickedId = $(this).attr("id").slice(4).charAt(0).toUpperCase() + $(this).attr("id").slice(5) + " ";
     newPizza.size = clickedId
+    console.log("Meddddium");
     $("#output-text-area").text(clickedId);
     $("#btn-small, #btn-medium, #btn-large, #btn-XL").hide();
     $("#btn-right, #btn-add, #btn-checkout, #btn-toppings").show(1000);
@@ -50,8 +50,6 @@ $(document).ready(function () {
   $("#btn-right").click(function (event) {
     $("#topping-text-area").text(toppingArr[clickCounter]) ;
     clickCounter++;
-    console.log(clickCounter.length);
-    console.log(toppingArr.length);
     if (clickCounter >= toppingArr.length) {
       clickCounter = 0;
     }
@@ -64,8 +62,8 @@ $(document).ready(function () {
   });
 
   $("#btn-checkout").click(function (event) {
-    console.log(`Made a ${newPizza.size} pizza with ${newPizza.toppings}. Total Cost: ${newPizza.pizzaCost()}`);
-
+    console.log(`Created a ${newPizza.size}pizza with ${newPizza.toppings}. Total Cost: ${newPizza.pizzaCost()}`);
+    newPizza.totalCost = 0;
   });
 
   $("#btn-toppings").click(function (event) {
