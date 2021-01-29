aa7343d0b8cb5b604d6dde20e7a572cea94d0390
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-29 14:07:59 -0800

    clean up code and further add functionality to buttons

--- js/scripts.js
+++ js/scripts.js
@@ -33,23 +33,23 @@ console.log(newPizza)
 
 
 $(document).ready(function () {
+  $("#topping-text-area").text(toppingArr[clickCounter]) 
+  $("#btn-toppings").hide();
   $("#btn-small, #btn-medium, #btn-large, #btn-XL").fadeIn(1500);
   $("#btn-size").click(function (event) {
     event.preventDefault();
-
-    $("#btn-left, #btn-right").hide();
-
+    $("#btn-left, #btn-right, #btn-add, #btn-checkout").hide();
   });
 
   $("#btn-small, #btn-medium, #btn-large, #btn-XL").click(function (event) {
     let clickedId = $(this).attr("id").slice(4).charAt(0).toUpperCase() + $(this).attr("id").slice(5) + " ";
     $("#output-text-area").append(clickedId);
-    $("#btn-small, #btn-medium, #btn-large, #btn-XL, #btn-size").hide();
-    $("#btn-left, #btn-right, #btn-add #btn-checkout").fadeIn(1000);
+    $("#btn-small, #btn-medium, #btn-large, #btn-XL").hide();
+    $("#btn-right, #btn-add, #btn-checkout, #btn-toppings").show(1000);
   });
 
   $("#btn-right").click(function (event) {
-    $("#topping-text-area").text(toppingArr[clickCounter] + " ") ;
+    $("#topping-text-area").text(toppingArr[clickCounter]) ;
     clickCounter++;
     console.log(clickCounter.length);
     console.log(toppingArr.length);
