27d5d8835e3ddab3389429e8cce6a06170d68e47
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-29 09:16:16 -0800

    passes first test correctly

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,23 @@
 function Pizza(topping, size, ) {
 	this.topping = topping;
   this.size = size;
-  this.toppings = ["cheese"];
+  this.toppings = ["cheese", "cheese"];
   this.totalCost = 0;
 }
 
 Pizza.prototype.pizzaCost = function() {
-  for (i = 0; i < this.toppings.length; i++)
+  for (i = 0; i < this.toppings.length; i++){
   console.log("hello");
-  this.totalCost = 3;
+  this.totalCost = this.totalCost + 3;
   console.log(this.totalCost)
-  
+}
 
 }
 
 //UI Logic
 let firstPizza = new Pizza("cheese", "medium")
 firstPizza.pizzaCost();
+console.log(firstPizza)
 
 
 
