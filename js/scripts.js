ff50705381f355d3a8db76f2dc8f58656411f670
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-29 12:56:49 -0800

    pull button id and append to output text area

--- js/scripts.js
+++ js/scripts.js
@@ -23,9 +23,11 @@ Pizza.prototype.createPizza = function () {
 
 }
 //UI Logic
-let firstPizza = new Pizza("cheese", "medium")
-firstPizza.pizzaCost();
-console.log(firstPizza)
+let currentSize = ""
+let toppingArr = []
+let newPizza = new Pizza("cheese", "medium")
+newPizza.pizzaCost();
+console.log(newPizza)
 
 
 
@@ -33,10 +35,18 @@ $(document).ready(function () {
 
   $("#btn-size").click(function (event) {
     event.preventDefault();
-    $("#btn-small, #btn-medium, #btn-large, #btn-xl").show();
+    $("#btn-small, #btn-medium, #btn-large, #btn-XL").show();
+    $("#btn-left, #btn-right").hide();
+
+  });
+
+  $("#btn-small, #btn-medium, #btn-large, #btn-XL").click(function (event) {
+    let clickedId = $(this).attr("id").slice(4).charAt(0).toUpperCase() + $(this).attr("id").slice(5);
+    $("#output-text-area").text(clickedId);
   });
   $("#btn-toppings").click(function (event) {
     event.preventDefault();
-    $("#btn-small, #btn-medium, #btn-large, #btn-xl").hide();
+    $("#btn-small, #btn-medium, #btn-large, #btn-XL").hide();
+    $("#btn-left, #btn-right").show();
   });
 });
