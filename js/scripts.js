a3db15a1ac6087a46ca9d84558711616a418e093
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-29 09:12:41 -0800

    pass first test

--- js/scripts.js
+++ js/scripts.js
@@ -3,20 +3,21 @@ function Pizza(topping, size, ) {
 	this.topping = topping;
   this.size = size;
   this.toppings = ["cheese"];
+  this.totalCost = 0;
+}
+
+Pizza.prototype.pizzaCost = function() {
+  for (i = 0; i < this.toppings.length; i++)
+  console.log("hello");
+  this.totalCost = 3;
+  console.log(this.totalCost)
+  
 
 }
 
-  Pizza.prototype.pizzaCost = function() {
-  for (i = 0; i >= this.toppings.length; i++)
-  {
-    console.log("Failed test")
-  }
-}
-
-
-//Testing constructor / prototypes (dev)
-let firstPizza = new Pizza("cheese", "medium")
-
-console.log(firstPizza)
-console.log(firstPizza.toppings.length)
 //UI Logic
+let firstPizza = new Pizza("cheese", "medium")
+firstPizza.pizzaCost();
+
+
+
