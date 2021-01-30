ca6f59af78c6d2ea7308aa447b2921d34e697397
Connor Burgess <connorburgesscodes@gmail.com>
2021-01-29 21:46:36 -0800

    resolve index out of bounds bug on counterArray

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ function Pizza() {
   this.toppings = [];
   this.totalCost = 0;
 }
+
 Pizza.prototype.pizzaCost = function () {
   for (i = 0; i < this.toppings.length; i++) {
     this.totalCost = this.totalCost + 3;
@@ -18,28 +19,31 @@ Pizza.prototype.pizzaCost = function () {
 //UI Logic
 $(document).ready(function () {
   $("#output-text-area").text(`What size pizza tickles your fancy?`);
-  let toppingArr = ["cheese", "pepperoni", "olives", "fried chicken", "banana", "chicken livers", "hot pocket", "mushrooms", "candy canes", "artichoke", "anchovy", "pineapple"];
+  const toppingArr = ["cheese", "pepperoni", "olives", "fried chicken", "banana", "chicken livers", "hot pocket", "mushrooms", "candy canes", "artichoke", "anchovy", "pineapple"];
   let newPizza = new Pizza();
   let clickCounter = 0;
-  $("#btn-toppings").hide();
+  $("#btn-toppings, #btn-size").hide();
   $(".jumbotron").fadeIn(2500);
   $("#btn-small, #btn-medium, #btn-large, #btn-XL").fadeIn(1500);
+
   $("#btn-size").click(function (event) {
     event.preventDefault();
     $("#btn-small, #btn-medium, #btn-large, #btn-XL").fadeIn(2500);
     $("#btn-left, #btn-right, #btn-add, #btn-checkout").hide();
   });
+
   $("#btn-small, #btn-medium, #btn-large, #btn-XL").click(function (event) {
     let clickedId = $(this).attr("id").slice(4).charAt(0).toUpperCase() + $(this).attr("id").slice(5);
     newPizza.size = clickedId;
     $("#btn-small, #btn-medium, #btn-large, #btn-XL").hide();
-    $("#btn-right, #btn-add, #btn-checkout, #btn-toppings").show(1000);
+    $("#btn-right, #btn-add, #btn-checkout, #btn-toppings, #btn-size").show(1000);
     return (newPizza.size === "Small" ? $("#output-text-area").text(`Big pizza in a ${clickedId} city!`)
       : newPizza.size === "Medium" ? $("#output-text-area").text(`Strike a happy ${clickedId}!`)
         : newPizza.size === "Large" ? $("#output-text-area").text(`${clickedId} and in charge!`)
           : newPizza.size === "XL" ? $("#output-text-area").text(`${clickedId}, the best size in anything!`)
             : newPizza.size);
   });
+
   $("#btn-right").click(function (event) {
     $("#output-text-area").text(`Would you like some ${toppingArr[clickCounter]} on that?`);
     clickCounter++;
@@ -47,19 +51,33 @@ $(document).ready(function () {
       clickCounter = 0;
     }
   });
+
   $("#btn-add").click(function (event) {
-    $("#output-text-area").text(`Added ${toppingArr[clickCounter - 1]} to the pizza!`);
-    newPizza.toppings.push(" " + toppingArr[clickCounter - 1] + " ");
+    //Ensure index is not out of bounds when array clickCounter resets
+    if (toppingArr[clickCounter - 1] != undefined) {
+      $("#output-text-area").text(`Added ${toppingArr[clickCounter - 1]} to the pizza!`)
+        (newPizza.toppings.push(" " + toppingArr[clickCounter - 1]) + " ")
+    }
+    else if (toppingArr[clickCounter] != toppingArr[clickCounter[-1]]) {
+      $("#output-text-area").text(`Added ${toppingArr[clickCounter + 11]} to the pizza!`)
+        (newPizza.toppings.push(" " + toppingArr[clickCounter + 11]) + " ")
+    }
+    else {
+      $("#output-text-area").text(`Added ${toppingArr[clickCounter]} to the pizza!`)
+      newPizza.toppings.push(" " + toppingArr[clickCounter])
+    }
   });
+
   $("#btn-checkout").click(function (event) {
     let pizzaPrice = newPizza.pizzaCost();
-    $("#final-text-area").append(`<li> Bought a ${newPizza.size} pizza with${newPizza.toppings.length >= 1 ? newPizza.toppings : " no ingredients"}. Total Cost: ${pizzaPrice}</li>`);
+    $("#final-text-area").append(`<li> Bought a ${newPizza.size} pizza with${newPizza.toppings.length >= 1 ? newPizza.toppings : " no ingredients"}. Total Cost: $${pizzaPrice}</li>`);
     $("#output-text-area").text(`Just bought a pizza for $${pizzaPrice}, dude!`);
     newPizza.totalCost = 0;
     newPizza.toppings = [];
     $("#btn-small, #btn-medium, #btn-large, #btn-XL").fadeIn(1500);
     $("#btn-left, #btn-right, #btn-add, #btn-checkout").hide();
   });
+
   $("#btn-toppings").click(function (event) {
     event.preventDefault();
     $("#btn-small, #btn-medium, #btn-large, #btn-XL").hide();
